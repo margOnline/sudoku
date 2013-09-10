6c5de09c3441601f3759a08d0abb95c5a1fad286
Margo <margo@margonline.co.uk>
2013-09-10 16:23:54 +0100

    added context to tests

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -2,18 +2,13 @@ class Sudoku
 
   attr_accessor :board
   ALLOWED_NUMS = [1,2,3,4,5,6,7,8,9]
+  
   def initialize
-    @board = [
-               ['','','','','','','','',''],
-               ['','','','','','','','',''],
-               ['','','','','','','','',''],
-               ['','','','','','','','',''],
-               ['','','','','','','','',''],
-               ['','','','','','','','',''],
-               ['','','','','','','','',''],
-               ['','','','','','','','',''],
-               ['','','','','','','','','']
-              ]
+    @board = Array.new(9){Array.new(9){|index| index =''}}
+  end
+
+  def play
+
   end
 
   def numbers_one_to_nine_in? row
@@ -22,8 +17,13 @@ class Sudoku
   end
 
   def no_duplicates_in? row
-    row.uniq.size == 1
+    row.uniq.size == row.length
   end
+
+  def rotate column
+    column.transpose
+  end
+
 
 
 end
--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -8,7 +8,7 @@ describe Sudoku do
      expect(sudoku.board).to be_a Array
     end
 
-    it 'will not have cells greater indexed greater than 9' do
+    it 'cells indexed less than 10' do
       expect{sudoku.board[9][10]}.to raise_error NoMethodError
     end
 
@@ -18,13 +18,40 @@ describe Sudoku do
     end
   end
 
-  it 'accepts only numbers 1-9 in cells' do
-    row = [1,2,3,4,5,6,7,8,9]
-    expect(sudoku.numbers_one_to_nine_in? row).to be_true
+  context 'accepts only' do
+    it 'numbers 1-9 in cells' do
+      row = [1,2,3,4,5,6,7,8,9]
+      expect(sudoku.numbers_one_to_nine_in? row).to be_true
+    end
+
+    it 'unique numbers in rows' do 
+      row = [1,2,3,4,5,6,7,8,9]
+      expect(sudoku.no_duplicates_in? row).to be_true
+    end 
   end
 
-it 'rejects numbers 1-9 in cells' do
-    row = [1,2,3,14,5,6,7,8,10]
-    expect(sudoku.numbers_one_to_nine_in? row).to be_false
+  context 'rejects' do  
+    it 'numbers in cells not in range 1-9' do
+      row = [1,2,3,14,5,6,7,8,10]
+      expect(sudoku.numbers_one_to_nine_in? row).to be_false
+    end
+
+    it 'non-unique numbers in rows' do 
+      row = [1,2,2,4,5,6,7,8,5]
+      expect(sudoku.no_duplicates_in? row).to be_false
+    end 
   end
+
+  it 'transposes columns into rows' do 
+    column = [[1,2],[3,4]]
+    expect(sudoku.rotate column).to eq [[1,3],[2,4]]
+  end 
+
+  it 'indexes each 3 x 3 sub box' do
+    expect(sudoku.add_box_index_to cell)
+  end
+
+
+
+
 end
